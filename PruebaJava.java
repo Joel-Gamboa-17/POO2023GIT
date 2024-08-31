6fa3986efb92dd7a99b5c78a5757dda2e013e338
JoelGamboa777 <joelgamboa777@gmail.com>
2024-08-31 17:47:58 -0500

    Primer commit de prueba

--- PruebaJava.java
+++ PruebaJava.java
@@ -1,5 +1,5 @@
 public class PruebaJava {
      public static void main(String[] args){
-         System.out.println("Hola Mundo");
+         System.out.println("Esto es solo de prueba");
       }
 }
